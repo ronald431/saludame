6b2d8b56c592c3cee906a101463fe0d2623156fe
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100215-Ronald.local>
2018-03-05 20:28:14 -0500

    Saludame

--- solution.rb
+++ solution.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 get '/' do
-  unless params[:nombre]
-    "<h1>Hola desconocido!</h1>"
-  else
+  if params[:nombre]
     "<h1>Hola #{params[:nombre]}!</h1>"
+  else
+    "<h1>Hola desconocido!</h1>"
   end
 end
